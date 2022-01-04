53719ab2171ed1bdd2a6fd57b6571ace5b56ed3a
Thomas Hansen <thomas@gaiasoul.com>
2022-01-04 07:45:58 +0200

    ougoug

--- assets/logic.js
+++ assets/logic.js
@@ -1,9 +1,10 @@
 
-function toggleMenu() {
+function toggleMenu(event) {
   var banner = document.getElementById('banner');
   if (banner.className == 'banner') {
     banner.className = 'banner expanded';
   } else {
     banner.className = 'banner';
   }
+  event.preventDefault();
 }
